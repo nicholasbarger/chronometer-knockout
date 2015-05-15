9811e38f48837fabf24ed045ec7b3ee1f2ed30c1
Nicholas Barger <nicholas@nicholasbarger.com>
2015-05-15 10:19:18 -0400

    Init.

---
+++ Scripts/chronometer-knockout/chronometer-knockout.js
@@ -0,0 +1,34 @@
+﻿ko.bindingHandlers.chronometer = {
+    init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+        var value = ko.unwrap(valueAccessor());
+        var options = prepOptions(value);
+
+        $(element).chronometer(value, options);
+
+        document.addEventListener(element.id + "_updated", function (e) {
+            var accessor = valueAccessor();
+            accessor.value(e.val);
+        });
+    },
+    update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+        var value = valueAccessor();
+        var valueUnwrapped = ko.unwrap(value);
+        var options = prepOptions(value);
+
+        $(element).chronometer(valueUnwrapped, options);
+    }
+}
+
+function prepOptions(value) {
+    var options = {};
+    if (value.inputFormat != null) {
+        options.inputFormat = value.inputFormat;
+    }
+    if (value.outputFormat != null) {
+        options.outputFormat = value.outputFormat;
+    }
+    if (value.includeTime != null) {
+        options.includeTime = value.includeTime;
+    }
+    return options;
+}
---
+++ Scripts/chronometer-knockout/examples-app.js
@@ -0,0 +1,5 @@
+﻿var viewModel = {
+    start: ko.observable('2015-05-06'),
+    end: ko.observable('2015-09-1 2:00')
+};
+ko.applyBindings(viewModel);
